a0260c871631ac2763ea64b35239e5aa326a8c1a
Akshaya192 <96040710+Akshaya192@users.noreply.github.com>
2022-05-25 10:25:30 +0000

    score

--- ActivitySet01/problem02.py
+++ ActivitySet01/problem02.py
@@ -1,3 +1,14 @@
 # Why Program
 
-print("hello world")
+x = 5
+if x > 2 :
+     print('Bigger than 2')
+     print('Still bigger')
+print('Done with 2')
+
+for i in range(5) :
+     print(i)
+     if i > 2 : 
+        print('Bigger than 2')
+     print('Done with i', i)
+print('All Done') 
