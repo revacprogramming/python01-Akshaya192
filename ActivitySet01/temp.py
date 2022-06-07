77688ef26b857b5c093c31527272f6a4161f02d5
Akshaya192 <96040710+Akshaya192@users.noreply.github.com>
2022-06-07 16:58:16 +0000

    str 01

---
+++ ActivitySet01/temp.py
@@ -0,0 +1,44 @@
+l=[]
+l.append(5)
+print(l)
+x=[1,2,3,45,6,7,]
+print(x)
+y=x.copy()
+print(y)
+x=[1,2,2]
+print(x.pop(1))
+print(x)
+x=[1,2,1,3]
+print(x.count(1))
+print(x)
+big=max('Hello world')
+print(big)
+tiny=min('Helloworld')
+print(tiny)
+fruit = 'banana'    
+letter = fruit[1]
+print(letter)
+x=3
+w=fruit[x-1]
+print(w)
+print(len(fruit))
+index=0
+while index<len(fruit):
+  letter=fruit[index]
+  print(index,letter)
+  index=index+1
+for letter in fruit:
+  print(letter)
+  word ='reva'
+  count =0
+for letter in word:
+  if letter=='a':
+      count =count+1
+print(count)
+fruit = 'banana'
+'n' in fruit
+'m' in fruit
+'nan' in fruit
+if 'a' in fruit:
+  print('Found it!')
+
