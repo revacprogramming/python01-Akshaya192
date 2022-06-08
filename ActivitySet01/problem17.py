b987051b8d68eb45e66eadc86a36d20fe1e40091
Akshaya192 <96040710+Akshaya192@users.noreply.github.com>
2022-06-08 09:45:38 +0000

    romeo.txt

---
+++ ActivitySet01/problem17.py
@@ -0,0 +1,3 @@
+str1='But soft what light through yonder window breaksIt is the east and Juliet is the sunArise fair sun and kill the envious moonWho is already sick and pale with grief'
+x=str1.split('a')
+print(x)
